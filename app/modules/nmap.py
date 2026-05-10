5f374db886264ae6c5aa5062532f606d925229b9
flo7716 <florian.andre.77@gmail.com>
2026-05-10 17:38:51 +0200

    ajout parsing

--- app/modules/nmap.py
+++ app/modules/nmap.py
@@ -1,5 +1,25 @@
 import subprocess
+import xml.etree.ElementTree as ET
 
+
+def parse_nmap(xml_output):
+
+    root = ET.fromstring(xml_output)
+
+    results = []
+
+    for port in root.findall(".//port"):
+        port_id = port.get("portid")
+        state = port.find("state").get("state")
+        service = port.find("service").get("name")
+
+        results.append({
+            "port": port_id,
+            "state": state,
+            "service": service
+        })
+
+    return results
 
 def run_nmap(target):
 
@@ -18,4 +38,4 @@ def run_nmap(target):
         text=True
     )
 
-    return result.stdout
+    return parse_nmap(result.stdout)
