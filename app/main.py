1dfce79dd0066c6dc5b9819208b52b61662c94ce
flo7716 <florian.andre.77@gmail.com>
2026-05-12 23:20:25 +0200

    ajout uvicorn

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from app.routes import router
 
-CORSMiddleware.add_middleware(
+
+app = FastAPI()
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(router)
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
